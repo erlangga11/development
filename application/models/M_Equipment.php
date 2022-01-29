a4d07b3c3a13e514a3f545dd53d322db47ff6363
Andrisyarif02 <72589273+Andrisyarif02@users.noreply.github.com>
2022-01-30 03:27:29 +0700

    revisi eq

--- application/models/M_Equipment.php
+++ application/models/M_Equipment.php
@@ -9,17 +9,16 @@ class M_Equipment extends CI_Model {
 		// $this->db->from('equipment');
 		// $query=$this->db->get();
 		// return $query->result();
-        
+        $total_rows = $this->db->affected_rows();
         if ($keyword) {
             $searchResult = $this->db->like('diskripsi',$keyword)->or_like('equipno',$keyword);
+            $total_rows = $searchResult->affected_rows();
         } 
-        $total_rows = $searchResult->affected_rows();
         $var = $this->db->limit($limit, $start)->get('equipment');
         $dataResult = $var->result();
         // echo "<pre>"; var_dump($total_rows); die;
         // echo "<pre>"; print_r(array($searchResult, $dataResult, $total_rows)); die;
-        return array($dataResult, $total_rows);
-        
+        return array($dataResult, $total_rows);      
 	}
     public function getRow($limit, $start){
         return $this->db->get('equipment',$limit, $start)->result_array();
